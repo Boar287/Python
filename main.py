51b7c0204020531e131566e72b14e76366409a5c
timofejdudic <Aqw23tr345>
2021-08-15 17:21:10 +0300

    Program which juxtaposes two emails with each other from one csv file

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
-import subprocess
+#!/usr/bin/env python3
+import csv
+import random
+import sys
 
-print("First line")
-subprocess.run(['sleep','2'])
-print("Second line")
+with open(sys.argv[1]) as f:
+    csv.register_dialect("my_dialect", skipinitialspace=True)
+    reader = csv.DictReader(f, dialect="my_dialect")
+    senders = list()
+    receivers = list()
+    emails = dict()
+    for line in reader:
+        senders.append(line["Name"])
+        receivers.append(line["Name"])
+        emails[line["Name"]] = line["Email"]
+
+
+def create_file_of_presents(senders, receivers):
+    HappyOfParticipants = len(senders)
+    dict_of_participants = dict()
+    for i, j in generate_random_lists(HappyOfParticipants):
+        dict_of_participants[senders[i]] = receivers[j]
+    return dict_of_participants
+
+
+def generate_random_lists(length):
+    first_list = list(range(length))
+    second_list = list(range(length))
+    while equality_of_elems(first_list, second_list):
+        random.shuffle(first_list)
+        random.shuffle(second_list)
+    return zip(first_list, second_list)
+
+
+def equality_of_elems(first_list, second_list):
+    for i in range(len(first_list)):
+        if first_list[i] == second_list[i]:
+            return True
+    return False
+
+
+with open(sys.argv[2], "w", newline='') as f:
+    keys = ["Sender", "Sender's e-mail", "Receiver"]
+    writer = csv.DictWriter(f, fieldnames=keys)
+    writer.writeheader()
+    people = create_file_of_presents(senders, receivers)
+    for i in range(len(senders)):
+        writer.writerow({keys[0]: list(people)[i], keys[1]: emails[senders[i]], keys[2]: people[senders[i]]})
